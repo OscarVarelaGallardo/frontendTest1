54357de4259b88b4009f4de77dfdec41ad7b2b10
Oscar Varela <oscar.varela@tilatina.com>
2025-06-25 17:08:53 -0600

    Enhances Order Details page with new styles and layout
    Improves the visual layout and styling of the Order Details page to provide a more user-friendly experience.
    
    Introduces a new container structure, updated card styles, and better alignment of elements. Additionally, adds new icons for status tracking and enhances the overall design with responsive adjustments.
    
    This update aims to enhance usability and visual appeal, making order tracking clearer for users.

--- src/pages/OrderDetails.tsx
+++ src/pages/OrderDetails.tsx
@@ -4,6 +4,11 @@ import bell from '../assets/icons/bell.svg';
 import circleBlue from '../assets/icons/circle-blue.svg';
 import circleGray from '../assets/icons/circle-gray.svg';
 import circleEmpty from '../assets/icons/circle-empty.svg';
+import line from '../assets/icons/line.svg'
+import checked from '../assets/icons/cheked.svg';
+import unchecked from '../assets/icons/unchecked.svg';
+import userPhoto from '../assets/img/user.png';
+import arrowUp from '../assets/icons/arrow-up.svg';
 
 import { useNavigate } from 'react-router-dom';
 import './OrderDetails.css';
@@ -14,70 +19,99 @@ const OrderDetails = () => {
     return (
 
         <>
-            <div className="orders-page-container-details">
-                <div className="orders-page-header">
-                    <img src={iconBack}
-                        onClick={() => { navigate('/') }}
-                        alt="Back Icon" className="back-icon" />
-                    <h2 className="orders-page-title">
-                        Cargo Details</h2>
-                    <img src={bell} alt="Bell Icon" className="icon-bell" />
+            <div className="order-details-container">
+                <div className="order-details-header">
+                    <img src={iconBack} onClick={() => navigate('/')} alt="Back Icon" />
+                    <h2 className="order-details-title">Cargo Details</h2>
+                    <img src={bell} alt="Bell Icon" />
                 </div>
-                <div className='order-page-card'>
-                    <div className='order-page-card-header'>
-                        <p className='order-page-card-header-title'>
-                            <span className='order-page-card-header-title-number'>
-                                Referencia A1190</span>
+
+                <div className="order-details-card">
+                    <div className="order-details-card-header">
+                        <p className="order-details-card-header-title">
+                            <span className="order-details-card-header-title-number">Referencia A1190</span>
                             <br />
                             Order Number #7804GNZ
                         </p>
-
                     </div>
-                    <div className='order-page-card-body'>
 
-
-                        <div className='order-page-card-body-image'>
-                            <img src={carYellow} alt="Car Yellow" className='car-yellow' />
-
-                            <img src={circleEmpty} alt="Circle Blue"
-
-                                className='circle-empty' />
-
-
+                    <div className="order-details-body">
+                        <div className="order-details-body-image">
+                            <img src={carYellow} alt="Car Yellow" />
+                            <img src={line} alt="line" className='line-separator' />
+                            <img src={circleEmpty} alt="Circle Empty" className="circle-drop" />
                         </div>
-                        <div className='order-page-card-body-items'>
-                            <p className='order-page-card-body-items-header'>PICKUP
-                                <br />
-                                <span>NEW YORK</span>
-                                <br />
+
+                        <div className="order-details-body-items">
+                            <p className="order-details-location">
+                                PICKUP <br />
+                                <span>NEW YORK</span><br />
                                 1234 Street Name, New York, NY 10001
                             </p>
-                            <div className='order-page-card-body-items-time'>
-                                <img src={circleBlue} alt="Circle Blue" className='circle-blue' />
+
+                            <div className="order-details-status">
+                                <img src={circleBlue} alt="Circle Blue" className="order-details-circle-icon" />
                                 <p>Accepted</p>
                             </div>
-                            <p className='order-page-card-body-items-header'>DROPOFF
-                                <br />
-                                <span>NEW YORK</span>
-                                <br />
-                                1234 Street Name, New York, NY 10001
 
+                            <p className="order-details-location">
+                                DROPOFF <br />
+                                <span>NEW YORK</span><br />
+                                1234 Street Name, New York, NY 10001
                             </p>
-                            <div className='order-page-card-body-items-time'>
-                                <img src={circleGray} alt="Circle Gray " className='circle-blue' />
+
+                            <div className="order-details-status">
+                                <img src={circleGray} alt="Circle Gray" className="order-details-circle-icon" />
                                 <p>On hold</p>
                             </div>
-
                         </div>
 
+                    </div>
+
+                </div>
+
+                <div className="order-details-card-timeline" style={{ marginTop: '40px' }}>
+                    <div className="order-details-tracking-header">
+                        <img src={userPhoto} alt="User Icon" className="order-details-user-icon" />
+                    </div>
+
+                    <div className="order-details-tracking-time">
+                        <p>10:30pm </p>
+                    </div>
+                    <div className='order-details-tracking-status'>
+                        <div className='order-details-tracking-status-item'>
+                            <div className='circle-item'>
+                                <img src={checked} alt="Checked Icon" className='checked-icon' />
+                            </div>
+                            <div className='circle-item'>
+                                <img src={checked} alt="Checked Icon" className='checked-icon' />
+                            </div>
+                            <div className='circle-item'>
+                                <img src={checked} alt="Checked Icon" className='checked-icon' />
+                            </div>
+                            <img src={unchecked} alt="Unchecked Icon" className='unchecked-icon' />
+                        </div>
+                        <div className='order-details-tracking-status-item-text'>
+                            <p className='order-details-tracking-status-item-text'>Created Order</p>
+                            <p className='order-details-tracking-status-item-text'>Accepted Order</p>
+                            <p className='order-details-tracking-status-item-text'>Pickup set up by User</p>
+                            <p className='order-details-tracking-status-item-text'>Pickup Completed</p>
+                        </div>
 
                     </div>
+                    <div className='order-details-tracking-status-footer'>
+                        <p>Track Order</p>
+                    </div>
+                    
+                </div>
+                <div className='order-details-pickData'>
+                    <p>Pickup Data</p>
+                        <img src={arrowUp} alt="Arrow up" className='arrow-up' />
                 </div>
 
             </div>
 
         </>
-
     )
 }
 
