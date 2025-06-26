4ce80544d51cf06b55b48ea953ca19585f167bcb
Oscar Varela <oscar.varela@tilatina.com>
2025-06-26 14:11:36 -0600

    Adds redirects copying functionality and updates dependencies
    Implements a plugin to copy redirects during the build process, enhancing deployment flexibility.
    
    Updates `package.json` and `package-lock.json` to include new dependencies for improved security and type definitions.
    
    Ensures project consistency and prepares for future enhancements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const copyRedirectsPlugin = () => {
+  return {
+    name: 'copy-redirects',
+    closeBundle() {
+      const src = path.resolve(__dirname, '_redirects')
+      const dest = path.resolve(__dirname, 'dist/_redirects')
+      fs.copyFileSync(src, dest)
+    }
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), copyRedirectsPlugin()]
 })
